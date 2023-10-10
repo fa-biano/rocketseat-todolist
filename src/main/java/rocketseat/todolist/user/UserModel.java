50b208bd8521f140aad8671607c3e8dde34987a6
Fa-biano <fbiano.souza@gmail.com>
2023-10-09 22:22:56 -0300

    feat: create user controller

---
+++ src/main/java/rocketseat/todolist/user/UserModel.java
@@ -0,0 +1,32 @@
+package rocketseat.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+    
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserModel(String username, String name, String password) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+}
