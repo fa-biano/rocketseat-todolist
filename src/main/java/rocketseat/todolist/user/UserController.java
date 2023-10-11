6cf8e6ea0afb2f96e133fa67c16f9c988980f796
Fa-biano <fbiano.souza@gmail.com>
2023-10-10 21:55:53 -0300

    chore: config h2 database and UserRepository

--- src/main/java/rocketseat/todolist/user/UserController.java
+++ src/main/java/rocketseat/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package rocketseat.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
     
     @PostMapping
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Usuário já existe");
+        }
+        
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/rocketseat/todolist/user/UserModel.java
+++ src/main/java/rocketseat/todolist/user/UserModel.java
@@ -1,32 +1,29 @@
 package rocketseat.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
-    
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
-    public UserModel(String username, String name, String password) {
-        this.username = username;
-        this.name = name;
-        this.password = password;
-    }
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createAt;
 }
